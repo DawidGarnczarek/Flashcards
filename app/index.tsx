c1bd6aa7b0a7bf3a8197227353177a014ce84153
Dawid Garnczarek <dawid.garnczarek98@gmail.com>
2025-01-24 20:34:55 +0100

    Add plus icon button

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+export const options = {
+  headerShown: false,
+};
+
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello helo</Text>
+      <TouchableOpacity style={styles.floatingButton} onPress={() => alert("Button Pressed!")}>
+        <Icon name="add" size={30} color="#fff" />
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  floatingButton: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#007bff",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+  },
+});
