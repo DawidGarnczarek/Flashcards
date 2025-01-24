4675cd49f6004289dc686b03d14be594c2f891d8
Dawid Garnczarek <dawid.garnczarek98@gmail.com>
2025-01-24 20:41:05 +0100

    Setup prettier

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,17 @@ export const options = {
   headerShown: false,
 };
 
-import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
-
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Index() {
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text>Hello helo</Text>
-      <TouchableOpacity style={styles.floatingButton} onPress={() => alert("Button Pressed!")}>
+      <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => alert('Button Pressed!')}
+      >
         <Icon name="add" size={30} color="#fff" />
       </TouchableOpacity>
     </View>
@@ -22,18 +22,18 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5f5f5",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
   },
   floatingButton: {
     width: 60,
     height: 60,
-    backgroundColor: "#007bff",
+    backgroundColor: '#007bff',
     borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
     bottom: 20,
     right: 20,
   },
